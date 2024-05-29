241f56da54b72bf2498707b238769478b61f1a48
aterui <hanabi0111@gmail.com>
2024-05-29 09:39:09 -0400

    add: test file for nfcl()

---
+++ tests/testthat/test-nfcl.R
@@ -0,0 +1,41 @@
+
+# data generation ---------------------------------------------------------
+
+## set parameters
+## - foodweb
+n_sp <- round(runif(1, 1, 10))
+fwb <- matrix(0, n_sp, n_sp)
+for(i in seq_len(nrow(fwb) - 1)) {
+  fwb[i, i + 1] <- 1
+}
+
+fwb <- fwb + t(fwb)
+
+## - ecosystem structure
+L <- runif(1, 10, 100)
+lambda_b <- runif(1, 0.1, 1)
+h <- delta <- 1
+
+## - resource and propagules
+rsrc <- runif(1, 1, 10)
+g <- runif(1, 1, 10)
+
+## numerical
+y <- nfcl(foodweb = fwb,
+          lambda = lambda_b,
+          size = L,
+          h = h,
+          delta = delta,
+          rsrc = rsrc,
+          g = g,
+          mu0 = 0,
+          mu_s = 0,
+          mu_p = 0,
+          mu_c = 0,
+          rho = 0)
+
+# test --------------------------------------------------------------------
+
+test_that("nfcl() works", {
+  expect_equal(c(y), n_sp)
+})
