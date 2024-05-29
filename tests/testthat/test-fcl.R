b21c89dc3b359397dc3c5d9c36d84f01a141aa5a
aterui <hanabi0111@gmail.com>
2024-05-29 09:29:58 -0400

    add: test file for fcl()

---
+++ tests/testthat/test-fcl.R
@@ -0,0 +1,39 @@
+
+# data generation ---------------------------------------------------------
+
+## set parameters
+## - foodweb
+n_sp <- round(runif(1, 1, 10))
+fwb <- matrix(0, n_sp, n_sp)
+for(i in seq_len(nrow(fwb) - 1)) {
+  fwb[i, i + 1] <- 1
+}
+
+fwb <- fwb + t(fwb)
+
+## - ecosystem structure
+L <- runif(1, 10, 100)
+lambda_b <- runif(1, 0.1, 1)
+h <- delta <- 1
+
+## - resource and propagules
+rsrc <- runif(1, 1, 10)
+g <- runif(1, 1, 10)
+
+## analytical
+y <- fcl(foodweb = fwb,
+         lambda = lambda_b,
+         size = L,
+         h = h,
+         delta = rep(delta, 2),
+         rsrc = rsrc,
+         g = c(g, g),
+         mu_base = 0,
+         mu_cnsm = 0,
+         rho = c(0, 0))
+
+# test --------------------------------------------------------------------
+
+test_that("fcl() works", {
+  expect_equal(c(y), n_sp)
+})
